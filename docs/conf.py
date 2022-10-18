78cc7954ba237badd466e34f6a82298975e65202
Fabian Brussa <fbrussa@novix.com>
2022-10-18 17:56:16 -0300

    fix github button

--- docs/conf.py
+++ docs/conf.py
@@ -51,10 +51,10 @@ language="en"
 html_theme = 'sphinx_book_theme'
 
 html_theme_options = {
-    "repository_url": "https://github.com/pyplan/resources",
+    "repository_url": "https://github.com/pyplan/security",
     "use_repository_button": False,
     "use_issues_button": False,
-    "use_edit_page_button": True,
+    "use_edit_page_button": False,
     "path_to_docs": "docs",
     "home_page_in_toc": False,
     "extra_navbar": "",
@@ -63,7 +63,7 @@ html_theme_options = {
 
 html_title = ""
 html_favicon = "_static/favicon.ico"
-htmlhelp_basename = "PyplanResources"
+htmlhelp_basename = "Pyplan_Security"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
